662cfdd67d3220d30e71af0e40e886af3a228808
Forrest Jones <forrestjones2010@gmail.com>
2023-02-01 10:04:21 -0600

    GDC#%

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,10 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import App from "../components/App";
+import HomeTest from "./HomeTest.js";
+import NavBarTest from ".NavBarTest.js";
+import AboutTest from "./AboutTest.js";
+
 
 test("it renders without crashing", () => {
   try {
@@ -9,4 +13,4 @@ test("it renders without crashing", () => {
     throw new Error("Make sure to import the required components!");
   }
 });
-exoprt default Test;
+exoprt default AppTest;
